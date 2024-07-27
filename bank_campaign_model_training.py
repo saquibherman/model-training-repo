90cd9df0194ccda3b29f016e6378399201d47da7
Saquib Herman <herman.saquib@gmail.com>
2024-07-27 20:14:37 +0200

    Update bank_campaign_model_training.py

--- bank_campaign_model_training.py
+++ bank_campaign_model_training.py
@@ -16,6 +16,7 @@ from sklearn.pipeline import make_pipeline
 storage_client = storage.Client()
 bucket = storage_client.bucket("saq-ml-ops")
 
+
 def load_data(path):
     return pd.read_csv(path, sep=";")
 
